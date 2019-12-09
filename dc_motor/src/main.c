9b6f832abdcb38798b7773dbf1ab3500831a4536
Diego Ramirez <diegoramirez@email.arizona.edu>
2019-12-09 15:49:22 -0600

    Changed from using timer 5 pwm 1 to timer 5 pwm 2.

--- dc_motor/src/main.c
+++ dc_motor/src/main.c
@@ -5,10 +5,16 @@
 int main() {
 	int duty_cycle_percentage = 10;
 	
-	tim5_pwm1_setup();
-	tim5_pwm1_pin_setup();
-	tim5_pwm1_set_duty_cycle(duty_cycle_percentage);
+	tim5_pwm2_setup();                                 //Set up channel 2 on timer 5 as pwm
+	tim5_pwm2_pin_setup();                             // Set up pin A1 to output timer 5 channel 2 (PWM)
+	tim5_pwm2_set_duty_cycle(duty_cycle_percentage);   // Set duty cycle for pwm on timer 5 channel 2
 	
+	/*
+	
+	Need to buy external op amp due to gpio not being able to power motor and internal op amps inability to be used as buffers.
+	
+	*/
+
 	while(1){
 		
 	}
